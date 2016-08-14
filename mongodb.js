b5a22f87fe0432c07665495c4d6fd3c718a5c6a4
Christos <christos_zenonos@hotmail.com>
2016-08-14 01:05:32 +0100

    Mongodb progress. Need to clean up code and need to throttle insers and api call(s) but it's getting there.

--- mongodb.js
+++ mongodb.js
@@ -1,30 +1,108 @@
+"use strict";
+
+//Load wrappers and libraries
+var co = require("co");
+var request = require("co-request");
 var mongoose = require('mongoose');
+var _ = require('lodash')
+
+//Connect to stations database
 mongoose.connect('mongodb://localhost/stations');
-var stationSchema = mongoose.Schema({});
-
-
+var stationSchema = mongoose.Schema({'ICS Code': String, 'Journeys' : Array, 'notes' :  String});
 var ListOfStations = mongoose.model('list_of_stations', stationSchema);
 
-function * loopThroughListOfStations(listOfStations, mainStationIndex) {
-    for (var StationInformation of listOfStations){
-        yield StationInformation;
+
+const tflApiRequest = function* (apiParams) {
+    let from = apiParams.from;
+    let to = apiParams.to;
+    try {
+
+        let result = yield request(`https://api.tfl.gov.uk/journey/journeyresults/${from}/to/${to}`);
+        return result
     }
-    if(mainStationIndex in listOfStations){
-        mainStationIndex++;
-        yield* loopThroughListOfStations(listOfStations, mainStationIndex);
-
+    catch (e) {
+        return false;
     }
-}
 
-function returnStation(StationInformation){
 
-}
+};
+
 ListOfStations.find({}, function (err, listOfStations) {
-        var stationLoop = loopThroughListOfStations;
-        for (var n of stationLoop(listOfStations, 0) ) {
-            console.log(n);
+        for (let n of loopThroughListOfStations(listOfStations, 0) ) {
+            let apiParams = new TflApiObject(n[0]['ICS Code'], n[1]['ICS Code']);
+
+            co(function * () {
+                try {
+                    // console.log('am I yielding?')
+                    const value = yield tflApiRequest(apiParams);
+                    // console.log('Yes I am')
+
+                    var tfl = JSON.parse(value.body);
+                    var shortestJourney = _.minBy(tfl.journeys, function(o) { return o.duration; });
+                    var journeyObject = new JourneyObject(shortestJourney);
+                    ListOfStations.update({_id : n[0]._id},{ 'Journeys' : journeyObject}, function(err,affected) {
+                        console.log(err);
+                        console.log(affected);
+                    });
+
+                }
+                catch (e) {
+                    console.log('failed')
+                    console.log(e);
+                }
+            });
         }
     }
 );
 
+//todo fix this. (remove the 5 limiter)
+function * loopThroughListOfStations(listOfStations, mainStationIndex) {
+    for (var stationIndex in listOfStations){
+        if(stationIndex == mainStationIndex || stationIndex > 5){
+            continue;
+        }
+        yield [listOfStations[mainStationIndex], listOfStations[stationIndex]];
 
+    }
+    if(mainStationIndex in listOfStations){
+        if(mainStationIndex < 5){
+            mainStationIndex++;
+            yield* loopThroughListOfStations(listOfStations, mainStationIndex);
+        }
+
+    }
+}
+
+function TflApiObject(from, to) {
+    this.from = from;
+    this.to = to;
+}
+
+function JourneyObject(journeyObject) {
+
+    var destinationIcsCode = journeyObject.legs[journeyObject.legs.length - 1].arrivalPoint.icsCode;
+
+    this.journey = {}
+    this.journey[destinationIcsCode] = {}
+    this.journey[destinationIcsCode].duration = journeyObject.duration;
+    this.journey[destinationIcsCode].startDateTime = journeyObject.startDateTime;
+    this.journey[destinationIcsCode].arrivalDateTime = journeyObject.arrivalDateTime;
+    this.journey[destinationIcsCode].journeyLegs = {};
+
+    var journeyLegs = {};
+
+    _.forEach(journeyObject.legs, function(value, key) {
+        journeyLegs[key] = {
+            'stationName' : journeyObject.legs[key].departurePoint.commonName,
+            'duration' : journeyObject.legs[key].duration,
+            'summary' :journeyObject.legs[key].instruction.summary,
+            'departureTime' :journeyObject.legs[key].departureTime,
+            'arrivalTime' :journeyObject.legs[key].arrivalTime,
+            'stationArrival' :journeyObject.legs[key].arrivalPoint.commonName
+
+        }
+
+    });
+
+    this.journey[destinationIcsCode].journeyLegs = journeyLegs;
+}
--- server.js
+++ server.js
@@ -4,9 +4,8 @@
 
 
 module.exports = function(app, Router) {
-    app
-        .use(Router.routes())
-        .listen(3000);
+    app.use(Router.routes())
+    app.listen(3000);
 };
 
 
