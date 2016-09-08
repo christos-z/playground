4b3158e97d00b6e4b2367bd7c709828684ab287e
Christos <christos_zenonos@hotmail.com>
2016-09-08 22:19:25 +0100

    Code cleanup

--- tfl.js
+++ tfl.js
@@ -10,50 +10,9 @@ const tflApiUrl = `https://api.tfl.gov.uk/journey/journeyresults/`;
 
 const RateLimiter = require('limiter').RateLimiter;
 
-//Stations DB connection object.
-const StationsDB = {
-    DB: 'mongodb://localhost/stations',
-    Table: 'list_of_stations',
-    Schema: {
-        'ICS Code': String,
-        'Journeys' : Array,
-        'notes' :  String
-    }
-
-};
-
-
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/stations');
 
-// var Stations = mongoose.model('list_of_stations',  {
-//     'ICS Code': Number,
-//     'Journeys' : Array,
-//     'Notes' :  String
-// });
-//
-// var test = yield Stations.findOne({ 'ICS Code': 1000002 });
-// console.log(test);
-//
-//
-// Stations.findOne({ 'ICS Code': 1000002 }).exec()
-//     .then(function(station) {
-//         console.log(station);
-//         //This is where I'd save also
-//     })
-//     .catch(function(err){
-//         // just need one of these
-//         console.log('error:', err);
-//     });
-// // Kitten.find({_id : '1000002'});
-//
-// // Kitten.findOne({ 'ICS Code': '/^1000002/' }, callback);
-//
-// Stations.findOne({ 'ICS Code': 1000002 }).exec()
-//     .then(function(station) {
-//         console.log(station);
-//     });
-//
 
 //Selects the list_of_stations table from the stations DB.
 const Stations = mongoose.model('list_of_stations',  {
@@ -63,19 +22,21 @@ const Stations = mongoose.model('list_of_stations',  {
 });
 //retrieves every single station object stored inside the mongo database.
 Stations.find({}, function (err, listOfStations) {
-
         var tflStations = retrieveStationPairs(0, listOfStations);
         retrieveStationJourneyFromApi(tflStations);
     }
 );
 
 
-//Loops through the list of stations, retrived from mongo DB and retrieves an array of
-//each station together with the main station index. Once all adjoning stations have been retrieved
-//for the choosen station index, the station index value is incremented and the function is looped again
-//untill all stations and adjoining stations are retrieved.
+//Loops through the list of stations, retrieved from mongo DB and retrieves an array of
+//each station together with the main station index. Once all adjoining stations have been retrieved
+//for the chosen station index, the station index value is incremented and the function is looped again
+//until all stations and adjoining stations are retrieved.
+
+//TODO can this be refactored to not need any param inputs? It seems reduntant to pass through a
+//main station index if it's going to be incrementing through all station indexes anyway.
 function * retrieveStationPairs(mainStationIndex, listOfStations) {
-    var stationIndex = 0;
+
     for (var stationIndex in listOfStations){
         if(stationIndex == mainStationIndex){
             continue;
@@ -96,7 +57,7 @@ function * retrieveStationPairs(mainStationIndex, listOfStations) {
 function retrieveStationJourneyFromApi (tflStations) {
     var station = tflStations.next().value;
 
-    //Throttle the requests to alieviete strain on tfl's API server
+    //Throttle the requests to alleviate strain on tfl's API server
     var limiter = new RateLimiter(1, 3000);
     limiter.removeTokens(1, function() {
 
@@ -112,57 +73,37 @@ function retrieveStationJourneyFromApi (tflStations) {
                 var shortestJourney = _.minBy(parsedTflApiResponse.journeys, function(o) { return o.duration; });
                 var stationToUpdate = yield Stations.findOne({_id : station[0]._id});
 
-
-                // stationToUpdate.Notes = 'changing notes test';
-                // console.log(stationToUpdate);
-                // stationToUpdate.save();
-                // return;
                 if (!stationToUpdate.Journeys) {
                     stationToUpdate.Journeys = {};
                 }
 
                 var journeyObject = new JourneyObject(shortestJourney);
 
-                // stationToUpdate.Journeys[Object.keys(journeyObject)] = journeyObject;
-
+                var icsCode = Object.keys(journeyObject)[0];
                 var apiReturnedJourney = {};
-                apiReturnedJourney[Object.keys(journeyObject)[0]] = journeyObject[Object.keys(journeyObject)[0]];
+                apiReturnedJourney[icsCode] = journeyObject[icsCode];
                 stationToUpdate.Journeys = _.merge(apiReturnedJourney, stationToUpdate.Journeys);
-                stationToUpdate.save();
-                // try {
-                //     const savedStation = yield stationToUpdate.save();
-                // } catch (e) {
-                //     console.log(e);
-                //     throw Error('Mongoose couldn\'t save for some reason');
-                // }
-
-                // stations.update({_id : station[0]._id},
-                //     {
-                //         //TODO change this from a push to a proper update with ICS code as a key
-                //         $set: {
-                //             Journeys : journeyObject
-                //         }
-                //
-                //     },
-                //     {
-                //         upsert:true,
-                //         multi:true
-                //     },
-                //     function(err,affected) {
-                //     console.log(affected);
-                // });
+                try {
+                    //TODO instead of saving/overwriting the DB with the new set of data. We should check to see
+                    //if the new set of data has a shorter duration than the exisiting one. And only if it does should we
+                    //overwrite
+                    yield stationToUpdate.save();
+                } catch (e) {
+                    console.log(e);
+                    //TODO add proper error logging.
+                    throw Error('Mongoose couldn\'t save for some reason');
+                }
 
             }
             catch (e) {
                 //TODO catch this error properly and do something should this ooccur
-                //                console.log(e);
+                throw Error('Something has gone seriously wrong.');
             }
         });
     });
     if(tflStations.next().done == false){
         limiter.removeTokens(1, function() {
             retrieveStationJourneyFromApi(tflStations);
-            // return;
         });
     }
 }
