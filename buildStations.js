f15bc357750836b5ad65acc107c706a73c680a5f
Christos <christos_zenonos@hotmail.com>
2016-09-30 12:33:45 +0100

    Code refactor

--- buildStations.js
+++ buildStations.js
@@ -20,45 +20,35 @@ const TravelModel = mongoose.model('Travel',  {
 });
 
 co(function * () {
-
-    let params = {requestUrl : `${tflApiUrl}tube,tflrail,overground,national-rail`};
-    // let params = {requestUrl : `${tflApiUrl}tube`};
+    const params = {requestUrl : `${tflApiUrl}tube`};
+    let tflApiResponse;
+    let parsedTflApiResponse;
 
     try {
+        tflApiResponse = yield apiRequest(params);
+        parsedTflApiResponse = JSON.parse(tflApiResponse.body);
+    } catch(e) {
+        throw Error('Request failed'); // you can also do custom error for every type of error
+    }
 
-
-        let tflApiResponse = yield apiRequest(params);
-        var parsedTflApiResponse = JSON.parse(tflApiResponse.body);
-
-        //rewrite this to use native foreach funciton, so that a
-        //travelObject.save method can be yielded instead.
-        _.forEach(parsedTflApiResponse.stopPoints, function(value, key) {
-            var travelObject = new TravelModel({
-                '_id': value.icsCode,
-                'stopType': value.stopType,
-                'stationNaptan': value.stationNaptan,
-                'naptanId': value.naptanId,
-                'commonName': value.commonName,
-                'modes' : value.modes
-            });
-
-            try {
-                travelObject.save();
-
-            } catch (e)
-            {
-                console.log(e);
-                //TODO update if key already exists and new duration is less
-                //TODO add proper error logging.
-                throw Error('Mongoose couldn\'t save for some reason');
-            }
-    
+    const promises = parsedTflApiResponse.stopPoints.map((value, key) => {
+        var travelObject = new TravelModel({
+            '_id': value.icsCode,
+            'stopType': value.stopType,
+            'stationNaptan': value.stationNaptan,
+            'naptanId': value.naptanId,
+            'commonName': value.commonName,
+            'modes' : value.modes
         });
 
-    
-    }
-    catch (e) {
-        //TODO catch this error properly and do something should this occur
-        throw Error('Something has gone seriously wrong.');
-    }
+        return travelObject.save();
+    });
+
+    const result = yield promises;
+
+    return result;
+}).then(function (result) {
+    console.log(result);
+}, function (err) {
+    console.error(err.stack);
 });
--- tfl.js
+++ tfl.js
@@ -8,12 +8,20 @@ const apiRequest = require('./api');
 const JourneyObject = require('./models/JourneyObject');
 const tflApiUrl = `https://api.tfl.gov.uk/journey/journeyresults/`;
 
-const RateLimiter = require('limiter').RateLimiter;
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/stations');
+mongoose.connect('mongodb://localhost/workgo');
 
-const Joruney = mongoose.model('Journeys',  {
+const TravelModel = mongoose.model('Travels',  {
+    '_id': String,
+    'stopType': String,
+    'stationNaptan': String,
+    'naptanId': String,
+    'commonName': String,
+    'modes' : Array
+});
+
+const JourneyModel = mongoose.model('Journeys',  {
     '_id': String,
     'Destination From': String,
     'Destination To': String,
@@ -25,92 +33,33 @@ const Joruney = mongoose.model('Journeys',  {
     'Journey Legs': Object,
     'mode': Object
 });
+//
+co(function * () {
+    try {
+        var results = yield TravelModel.find({}).exec();
+    } catch(e) {
+        throw Error('Unable to retrieve travelObject from DB.');
+    }
+    return results;
 
-var from = 1000001;
-var to = 1000002;
-
-function retrieveStationJourneyFromApi () {
-
-    //Throttle the requests to alleviate strain on tfl's API server
-    var limiter = new RateLimiter(1, 1000);
-    limiter.removeTokens(1, function() {
-
-        let params = {requestUrl : `${tflApiUrl}${from}/to/${to}`};
-        co(function * () {
-            try {
-                let tflApiResponse = yield apiRequest(params);
-                var parsedTflApiResponse = JSON.parse(tflApiResponse.body);
-                var shortestJourney = _.minBy(parsedTflApiResponse.journeys, function(o) { return o.duration; });
-
-                var journeyLegs = {};
-
-                _.forEach(shortestJourney.legs, function(value, key) {
-
-                    journeyLegs[key] = {
-                        'stationName' : value.departurePoint.commonName,
-                        'duration' : value.duration,
-                        'summary' : value.instruction.summary,
-                        'departureTime' : value.departureTime,
-                        'arrivalTime' : value.arrivalTime,
-                        'stationArrival' : value.arrivalPoint.commonName,
-                        'mode' : {
-                            'type' : value.mode.type,
-                            'name' : value.mode.name,
-                            'id' : value.mode.id
-                        }
-
-                    }
-
-                });
-
-                console.log(journeyLegs);
-                var destinationFrom = shortestJourney.legs[0].departurePoint.commonName;
-                var destinationTo = shortestJourney.legs[shortestJourney.legs.length -1].arrivalPoint.commonName;
-
-                var testStation = new Joruney({
-                    '_id' : `${from}/${to}`,
-                    'Destination From' : destinationFrom,
-                    'Destination To' : destinationTo,
-                    'ICS Code From' : from,
-                    'ICS Code To' : to,
-                    'Duration' : shortestJourney.duration,
-                    'Start Date Time' : shortestJourney.departureTime,
-                    'End Date Time' : shortestJourney.arrivalTime,
-                    'Journey Legs' : journeyLegs
-                });
-
-                try {
-                    yield testStation.save();
-                    console.log(shortestJourney);
-
-                } catch (e)
-                {
-                    console.log(e);
-                    //TODO update if key already exists and new duration is less
-                    //TODO add proper error logging.
-                    throw Error('Mongoose couldn\'t save for some reason');
-                }
-
-            }
-            catch (e) {
-                //TODO catch this error properly and do something should this occur
-                throw Error('Something has gone seriously wrong.');
-            }
-        });
+}).then(function (results) {
+    const requests = results.map(value => {
+        // var from = value._id;
+        // results.map(value => {
+        //     var to = value._id;
+        //     if(from != to)
+        //     {
+        //
+        //         let params = {requestUrl : `${tflApiUrl}${from}/to/${to}`};
+        //         console.log(params);
+        //
+        //         // let tflApiResponse = apiRequest(params);
+        //         // var parsedTflApiResponse = JSON.parse(tflApiResponse.body);
+        //         // var shortestJourney = _.minBy(parsedTflApiResponse.journeys, function(o) { return o.duration; });
+        //     }
+        //
+        // })
     });
-
-
-    limiter.removeTokens(1, function() {
-        if(from == 1018000) {
-            from = 1000001
-            to++;
-        }else{
-            from++
-        }
-        if(to !== 1018000) {
-            retrieveStationJourneyFromApi();
-        }
-    });
-}
-
-retrieveStationJourneyFromApi();
+}, function (err) {
+    console.error(err.stack);
+});
